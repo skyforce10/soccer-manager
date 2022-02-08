e936768e619f6b38369cd1b7ad38608ad3bbbf55
mohamed el dakdouki <74624386+skyforce10@users.noreply.github.com>
2022-02-08 12:51:00 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import {Alarm, Heart, People, PeopleFill} from "react-bootstrap-icons";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Car from './Car/Car'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <container>
+          <Row>
+              <Col xs="auto"><button class="btn btn-primary box-button" onClick={Car}><People />Sign in</button></Col>
+              <Col xs="auto"><button class="btn btn-success" ><PeopleFill />Sign up</button></Col>
+          </Row>
+          </container>
+
       </header>
     </div>
   );
